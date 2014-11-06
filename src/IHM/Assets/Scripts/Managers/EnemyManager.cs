b79cc713391074d045a5edd0d59687703ee99bee
mathildeandre <Ma17th08theophile.plancke@ensimag.imag.fr>
2014-11-06 14:35:11 +0100

    Add project game for virtual reality

---
+++ src/IHM/Assets/Scripts/Managers/EnemyManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public GameObject enemy;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+
+
+    void Start ()
+    {
+		// repeat the spawn funtion every 3 sec
+        InvokeRepeating ("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn ()
+    {
+        if(playerHealth.currentHealth <= 0f)
+        {
+            return;
+        }
+
+        int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+
+		// Instanciate creates an instance of something 
+        Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
