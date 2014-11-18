60c1c22e28490a4b73837566deee7d31453ae629
Pandakls <theophile.plancke@ensimag.imag.fr>
2014-11-18 17:24:12 +0100

    IHM project

---
+++ src/IHM/Assets/LeapMotion/Leap_Boilerplate/scripts_for_example_scenes/FingerCountChecker.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FingerCountChecker : MonoBehaviour {
+
+
+	private LeapManager _leapManager;
+	private TextMesh text;
+
+	//Player manipulation
+	bool wasFive = false;
+	bool pinch = false;
+	bool isGrabbed = false;
+	Leap.Vector initialPinchPosition;
+	Leap.Vector initialHandOrientation;
+	Leap.Vector movement;
+	float angleOfRotation;
+	//PlayerMovement playermovment
+
+	// Use this for initialization
+	void Start () {
+		//Playermovement = player.getcomponent<PlayerMovement>();
+		text = gameObject.GetComponent(typeof(TextMesh)) as TextMesh;
+		_leapManager = (GameObject.Find("LeapManager") as GameObject).GetComponent(typeof(LeapManager)) as LeapManager;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "Finger Count: " + _leapManager.frontmostHand().Fingers.Count;
+
+		if (_leapManager.frontmostHand ().Fingers.Count == 5){ // Before pinch
+			wasFive = true;
+			pinch = false;
+			isGrabbed = false;
+			//Save and compare initial pinch position
+			initialPinchPosition = _leapManager.frontmostHand().PalmPosition;
+			initialHandOrientation = initialPinchPosition - _leapManager.frontmostHand().Fingers.Rightmost.StabilizedTipPosition;
+			initialHandOrientation.y = 0;
+			initialHandOrientation = initialHandOrientation.Normalized;
+		} else if (_leapManager.frontmostHand ().Fingers.Count == 0){ // out of screen
+			pinch = false;
+			wasFive = false;
+			isGrabbed = false;
+		} else if (wasFive && isPinchOnPlayer()){
+			isGrabbed = true;
+			wasFive = false;
+			pinch = true;
+		} else if (wasFive || pinch) { // pinch
+			pinch = true;
+			wasFive = false;
+		}
+		if (pinch && !isGrabbed) {
+			text.text += "\npinched but missed";
+		}
+		if (isGrabbed) {
+			text.text += "\nGrabbed!";
+			//Compute movement from initial position
+			movement = _leapManager.frontmostHand().PalmPosition - initialPinchPosition;
+			//Compute rotation from initial position
+			Leap.Vector finalHandOrientation = _leapManager.frontmostHand().PalmPosition - _leapManager.frontmostHand().Fingers.Rightmost.StabilizedTipPosition;
+			finalHandOrientation.y = 0;
+			finalHandOrientation = finalHandOrientation.Normalized;
+			angleOfRotation =  Mathf.Acos(initialHandOrientation.x*finalHandOrientation.x + initialHandOrientation.y*finalHandOrientation.y);
+
+			//PlayerMovement.Move (movement.x, movement.z);
+			//PlayerMovement.Rotate(angleOfRotation);
+
+			//Set new initial start for next iterations
+			initialHandOrientation = finalHandOrientation;
+			initialPinchPosition = _leapManager.frontmostHand().PalmPosition;
+
+		}
+
+	}
+
+	bool isPinchOnPlayer () {
+		return true;
+	}
+}
---
+++ src/IHM/Assets/LeapMotion/Leap_Boilerplate/scripts_for_example_scenes/SlideControler.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlideControler : MonoBehaviour {
+	public GameObject[] slides;
+	private LeapManager _leapManager;
+
+	bool rightMoveRequired = false;
+	bool leftMoveRequired = false;
+	bool fingerRecognition = true;
+	bool pageMoveRequired = false;
+
+	float horizontalSpeed = 0.0f;
+	float horizontalPosition = 0.0f;
+	float previousHorizontalPosition = 0.0f;
+
+	int actualSlide = 0;
+	int slideNumber = 0;
+
+	int sleepSlide = 0;
+	int sleepRecog = 0;
+	int sleepTime = 15;
+	int state = 0;
+
+	// Use this for initialization
+	void Start () {
+		_leapManager = (GameObject.Find("LeapManager") as GameObject).GetComponent(typeof(LeapManager)) as LeapManager;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		previousHorizontalPosition = horizontalPosition;
+		horizontalPosition = _leapManager.frontmostHand ().PalmPosition.x;
+		horizontalSpeed = horizontalPosition - previousHorizontalPosition;
+
+		if (sleepSlide <= 0){
+			if (horizontalSpeed < -21) {
+				rightMoveRequired = true;
+				sleepSlide = 2*sleepTime;
+			} else if (horizontalSpeed > 21) {
+				leftMoveRequired = true;
+				sleepSlide = 2*sleepTime;
+			}
+		}else {
+			sleepSlide -=1;
+		}
+
+		if(sleepRecog <= 0 && horizontalSpeed < 0.5 && horizontalSpeed > -0.5){
+			pageRecognition(); //Machine à etat
+			sleepRecog = sleepTime;
+		}else {
+			sleepRecog --;
+		}
+		
+		if(rightMoveRequired ){
+			moveRight();
+			Debug.Log("Move Right\n");
+			rightMoveRequired = false;
+		}
+		if (leftMoveRequired ) {
+			moveLeft();
+			Debug.Log("Move Left\n");
+			leftMoveRequired = false;
+		}
+		if (pageMoveRequired) {
+			if(slideNumber > 0){
+				moveTo (slideNumber);
+				Debug.Log("Move to Page"+slideNumber+ "\n");
+				pageMoveRequired = false;
+			}
+		}
+	}
+
+	void moveRight(){
+		if (actualSlide + 1 < slides.GetLength(0)) {
+			actualSlide += 1;
+			moveTo(actualSlide);
+		}
+	}
+
+	void moveLeft(){
+		if (actualSlide > 0) {
+			actualSlide -= 1;
+			moveTo(actualSlide);
+		}
+	}
+
+	void moveTo(int numSlide){
+		actualSlide = numSlide;
+		transform.Translate(-slides[actualSlide].transform.position.x, 0, 0);
+	}
+
+	void pageRecognition(){
+		if (fingerRecognition){
+			slideNumber = _leapManager.frontmostHand ().Fingers.Count;
+			if(slideNumber != 0){
+				fingerRecognition = false;
+			}
+		} else {
+			Debug.Log("page recog :"+slideNumber+"\n");
+			Debug.Log("State:"+state+"\n");
+			switch (state){
+			case 0:
+			case 1:
+			case 2:
+				if(_leapManager.frontmostHand ().Fingers.Count == slideNumber){
+					state += 1;
+				}else{
+					state = 0;
+					fingerRecognition = true;
+				}
+				break;
+			case 3:
+				if(_leapManager.frontmostHand ().Fingers.Count != slideNumber){
+					state = 0;
+					fingerRecognition = true;
+					pageMoveRequired = true;
+				}
+				break;
+			default:
+				break;
+			}
+		}
+	}
+
+}
